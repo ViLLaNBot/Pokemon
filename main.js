3ef0cf6bec8408ca7ceff5519e729fcfb0433d60
mckiernantim <mckiernantim@gmail.com>
2018-04-26 18:07:12 -0400

    added API request and stats update, with css coloring and backgrounds

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 $(document).ready(function (){
-    // create a LatLng object containing the coordinate for the center of the map
-    var latlng = new google.maps.LatLng(-33.86455, 151.209);
-    // prepare the map properties
-    var options = {
-      zoom: 15,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      navigationControl: true,
-      mapTypeControl: false,
-      scrollwheel: false,
-      disableDoubleClickZoom: true
-    };
-    // initialize the map object
-    var map = new google.maps.Map(document.getElementById('google_map'), options);
-    // add Marker
-    var marker1 = new google.maps.Marker({
-      position: latlng, map: map
-    });
-    // add listener for a click on the pin
-    google.maps.event.addListener(marker1, 'click', function() {
-      infowindow.open(map, marker1);
-    });
-    // add information window
-    var infowindow = new google.maps.InfoWindow({
-      content:  '<div class="info"><strong>This is my company</strong><br><br>My company address is here<br> 32846 Sydney</div>'
-    });
+
+  let haunter = {};
+  let groudon = {};
+  let absol = {};
+  $.get("https://pokeapi.co/api/v2/pokemon/093", function(haunterStats) {
+    console.log(haunterStats);
+    let haunterHp = $(".hp1");
+    let haunterAtk = $(".atk1");
+    let haunterDef=$(".def1");
+    
+    haunterHp.text("HP:"+ haunterStats.stats[5].base_stat);
+    haunterDef.text("ATK:"+ haunterStats.stats[4].base_stat);
+    haunterAtk.text("DEF:"+ haunterStats.stats[3].base_stat);
+    haunter = (haunterStats);
+    
+
   });
+  $.get("https://pokeapi.co/api/v2/pokemon/383", function(groudonStats) {
+    console.log(groudonStats);
+    let groudonHp = $(".hp2");
+    let groudonAtk = $(".atk2");
+    let groudonDef=$(".def2");
+    
+    groudonHp.text("HP:"+ groudonStats.stats[5].base_stat);
+    groudonDef.text("ATK:"+ groudonStats.stats[4].base_stat);
+    groudonAtk.text("DEF:"+ groudonStats.stats[3].base_stat);
+    groudon = groudonStats;
+ 
+ });
+ $.get("https://pokeapi.co/api/v2/pokemon/359", function(absolStats) {
+    console.log(absolStats);
+    let absolHp = $(".hp3");
+    let absolAtk = $(".atk3");
+    let absolDef=$(".def3");
+    
+    absolHp.text("HP:"+ absolStats.stats[5].base_stat);
+    absolDef.text("ATK:"+ absolStats.stats[4].base_stat);
+    absolAtk.text("DEF:"+ absolStats.stats[3].base_stat);
+    absol = (absolStats)
+ 
+ });
+
+
+
+});
